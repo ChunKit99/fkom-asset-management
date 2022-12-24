81b72bfba85f8a0a1339fe5bcf43383e08c5e8d3
LIEW CHUN KIT <61322992+ChunKit99@users.noreply.github.com>
2022-12-24 13:01:09 +0800

    create asset seeder

---
+++ database/seeders/assets.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Str;
+use Faker\Factory as Faker;
+use App\Models\User;
+use App\Models\vendors;
+class assets extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $faker = Faker::create();
+        foreach(range(1,5) as $value){
+            DB::table('assets') -> insert([
+                'serial_number' => $faker -> bothify('?#???###J#'),
+                //J2nsz429L8
+                'location' => $faker -> bothify('FSK##'),
+                //FSK12
+                'category' => $faker -> randomElement(['computer', 'equipment', 'laboratory']),
+                'budget' => $faker -> randomFloat('2', 0, 2),
+                'vendor_id' => User::all()->random()->id,
+                'user_id' => User::all()->random()->id,
+            ]);
+        }
+    }
+}
