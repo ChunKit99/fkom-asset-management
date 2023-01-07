2f8914b8d39876ee8c133b3d1bccf2194af90cb2
OrangeOng <bryan81238@gmail.com>
2023-01-07 12:14:44 +0800

    Done all function

--- resources/views/LocationManagement/viewLocationInfo.blade.php
+++ resources/views/LocationManagement/viewLocationInfo.blade.php
@@ -6,11 +6,11 @@
         <form action="{{ url('Location/'.$location->id) }}" method="post">
             {!! csrf_field() !!}
             @method("PATCH")
-            
+
+            <label>#</label>
             <input type="text" name="id" id="id" value="{{$location->id}}" id="id" class="form-control" readonly> <br>
             <label>Location</label>
             <input type="text" name="name" id="name" value="{{$location->name}}" class="form-control" readonly></br>
-            <a href="{{ url('LocationManagement') }}" title="Back" class="btn btn-info"> Back </a><br>
 
         </form>
     </div>
