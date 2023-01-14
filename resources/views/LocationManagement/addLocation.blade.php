1a1da464b2b43e4363dc5d8cccf21f89cef91729
OrangeOng <bryan81238@gmail.com>
2023-01-14 11:21:19 +0800

    route correction

--- resources/views/LocationManagement/addLocation.blade.php
+++ resources/views/LocationManagement/addLocation.blade.php
@@ -1,14 +1,21 @@
 @extends('layouts.master')
 @section('content')
-<div class="card">
-    <div class="card-header">LocationManagement</div>
-    <div class="card-body">
-        <form action="{{ url('LocationManagement') }}" method="post">
-            {!! csrf_field() !!}
-            <label>Location</label></br>
-            <input type="text" name="name" id="name" class="form-control"></br>
-            <input type="submit" name="Save" class="btn btn-success"></br>
-        </form>
+<div class="container">
+    <div class="row">
+        <div class="col-md-11 mx-auto">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Location Information</h4>
+                </div>
+                <div class="card-body">
+                    <form action="{{ url('LocationManagement') }}" method="post">
+                        {!! csrf_field() !!}
+                        <label><h6>Location</h6></label></br>
+                        <input type="text" name="name" id="name" class="form-control"></br>
+                        <input type="submit" name="Save" class="btn btn-success"></br>
+                    </form>
+                </div>
+            </div>
+        </div>
     </div>
-</div>
-@endsection
+    @endsection
--- resources/views/LocationManagement/viewLocationInfo.blade.php
+++ resources/views/LocationManagement/viewLocationInfo.blade.php
@@ -1,22 +1,27 @@
 @extends('layouts.master')
 @section('content')
-<div class="card">
-    <div class="card-header">Location Information</div>
-    <div class="card-body">
-        <form action="{{ url('Location/'.$location->id) }}" method="post">
-            {!! csrf_field() !!}
-            @method("PATCH")
+<div class="container">
+    <div class="row">
+        <div class="col-md-11 mx-auto">
+            <div class="card">
+                <div class="card-header">
+                    <h4>Location Information</h4>
+                </div>
+                <div class="card-body">
+                    <form action="{{ url('Location/'.$location->id) }}" method="post">
+                        {!! csrf_field() !!}
+                        @method("PATCH")
 
-            <label>#</label>
-            <input type="text" name="id" id="id" value="{{$location->id}}" id="id" class="form-control" readonly> <br>
-            <label>Location</label>
-            <input type="text" name="name" id="name" value="{{$location->name}}" class="form-control" readonly></br>
-            <div class="col-xs-12 col-sm-12 col-md-12 text-center">
-              <a class="btn btn-info" href="{{ url('LocationManagement') }}"> Back</a>
+                        <label><h6>Location</h6></label>
+                        <input type="text" name="name" id="name" value="{{$location->name}}" class="form-control" readonly></br>
+                        <div class="col-xs-12 col-sm-12 col-md-12 text-center">
+                            <a class="btn btn-info" href="{{ url('LocationManagement') }}"> Back</a>
+                        </div>
+                    </form>
+                </div>
             </div>
-        </form>
+        </div>
     </div>
 </div>
-
 
 @endsection
